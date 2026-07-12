5f1959877d4c74c44ec7d2b7b9d0e98c8899b6b0
gb <gabriellyv1244@gmail.com>
2026-07-11 23:50:36 -0300

    learning about strings

---
+++ 1-strings.js
@@ -0,0 +1,10 @@
+// Strings
+const nome = 'Gabrielly';
+const idade = 20;
+const string1 = "abobra";
+const string2 = 'abobra';
+const string3 = `abobra`;
+const string4 = `Olá ${nome}`;
+const string5 = 'Olá ' + nome;
+const string6 = `Olá ${nome} você tem ${idade} anos de idade`;
+const string7 = 'Olá ' + nome + ' você tem ' + idade + ' anos de idade';
