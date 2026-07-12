995b7b87cb795d89fc45ed2795bb4ae9997228f0
gb <gabriellyv1244@gmail.com>
2026-07-11 23:53:08 -0300

    Exercises on numbers and simple calculations

---
+++ 2-numbers.js
@@ -0,0 +1,22 @@
+// Numbers
+
+const integer1 = 10;
+const integer2 = 100;
+const integer3 = 1000;
+const integer4 = -1;
+
+// Sum +
+const sum = integer1 + integer3;
+console.log( `Sum: ${sum}` );
+
+// Subtraction -
+const subtraction = integer3 - integer1;
+console.log( `Subtraction: ${subtraction}` );
+
+// Multiplication *
+const multiplication = integer1 * integer2;
+console.log( `Multiplication: ${multiplication}` );
+
+// Division /
+const division = integer3 / integer1;
+console.log( `Division: ${division}` );
