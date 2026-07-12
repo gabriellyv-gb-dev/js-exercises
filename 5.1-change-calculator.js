03cef4fe4204c1d233c513cde969d5b7a0fc54b6
gb <gabriellyv1244@gmail.com>
2026-07-12 00:01:21 -0300

    supermarket purchasing system

---
+++ 5.1-change-calculator.js
@@ -0,0 +1,86 @@
+//Change Calculator
+
+const marketItems = [
+    {
+        "Name": "Banana",
+        "pricePerUnit": 2,
+        "Quantity": 10,
+    },
+    {
+        "Name": "Leite",
+        "pricePerUnit": 6.50,
+        "Quantity": 5,
+    },
+    {
+        "Name": "Pão",
+        "pricePerUnit": 1,
+        "Quantity": 8,
+    },
+    {
+        "Name": "Ração para Gato",
+        "pricePerUnit": 28,
+        "Quantity": 2,
+    },
+    {
+        "Name": "Arroz",
+        "pricePerUnit": 30,
+        "Quantity": 2,
+    }
+];
+
+function banana() {
+    const banana = marketItems[0];
+    const purchaseItem1 = banana.Name;
+    const itempricePerUnit1 = banana.pricePerUnit;
+    const itemQuantity1 = banana.Quantity;
+
+    const bananaItem = {
+        
+    }
+    
+}
+
+console.log( `${banana()}` )
+
+
+
+
+
+
+
+/*
+
+ let Leite = marketItems[1];
+    let purchaseItem2 = Leite.Name;
+    let itempricePerUnit2 = Leite.pricePerUnit
+    let itemQuantity2 = Leite.Quantity
+    return `Result: ${purchaseItem2}`;
+    return `Result: ${itempricePerUnit2}`;
+    return `Result: ${itemQuantity2}`;
+
+    let Pão = marketItems[2];
+    let purchaseItem3 = Pão.Name;
+    let itempricePerUnit3 = Pão.pricePerUnit
+    let itemQuantity3 = Pão.Quantity
+    return `Result: ${purchaseItem3}`;
+    return `Result: ${itempricePerUnit3}`;
+    return `Result: ${itemQuantity3}`;
+
+    let raçãoParaGato = marketItems[3];
+    let purchaseItem4 = raçãoParaGato.Name;
+    let itempricePerUnit4 = raçãoParaGato.pricePerUnit
+    let itemQuantity4 = raçãoParaGato.Quantity
+    return `Result: ${purchaseItem4}`;
+    return `Result: ${itempricePerUnit4}`;
+    return `Result: ${itemQuantity4}`;
+
+    let Arroz = marketItems[4];
+    let purchaseItem5 = Arroz.Name;
+    let itempricePerUnit5 = Arroz.pricePerUnit;
+    let itemQuantity5 = Arroz.Quantity;
+    return `Result: ${purchaseItem5}`;
+    return `Result: ${itempricePerUnit5}`;
+    return `Result: ${itemQuantity5}`;
+
+
+*/
